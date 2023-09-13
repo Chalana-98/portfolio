1593baca1174cd75bf5b0714ef0c443dae8aa72c
Chalana Shehara <73055643+Chalana-98@users.noreply.github.com>
2023-09-13 08:28:47 +0530

    Motion add
    Motion add

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,8 +1,12 @@
+"use client"
+
 import React from 'react'
 import { BsMoonStarsFill } from 'react-icons/bs'
 import Image from 'next/image'
+import { motion } from "framer-motion"
+import Typewriter from 'typewriter-effect'
 
-import { AiOutlineGithub, AiFillLinkedin, AiOutlineWhatsApp } from 'react-icons/ai'
+import { AiOutlineGithub, AiFillLinkedin, AiOutlineWhatsApp, AiOutlineMail } from 'react-icons/ai'
 
 function Hero() {
   return (
@@ -10,7 +14,7 @@ function Hero() {
     <section >
 
       <nav className='py-10 flex justify-between'>
-        <h1 className=' text-lg md:text-xl lg:xxl font-bold'>
+        <h1 className=' text-3xl md:text-3xl lg:3xl font-bold'>
           Develop By Chalana
         </h1>
         <ul className=' flex items-center'>
@@ -21,7 +25,7 @@ function Hero() {
           <li>
             <a href="https://drive.google.com/file/d/11YtBLUntv3yXo3lBgL6ew7plo-4_kjEl/view?usp=sharing"
               download={'Chalana CV'}
-              className=' bg-blue-500 text-black px-2 py-2 border-none rounded-2xl'
+              className=' bg-blue-500 text-black px-4 py-2 border-none rounded-xl'
             >Resume
             </a>
           </li>
@@ -30,10 +34,21 @@ function Hero() {
       {/* Hero */}
 
       <div className="flex flex-col text-center p-16 sm:flex-row">
-      <div className="sm:basis-1/2">
+        <div className="sm:basis-1/2">
           <h1 className='text-5xl font-sans font-bold text-blue-900 md:text-6xl lg:text-7xl'>I'm Chalana</h1>
-          <h2 className='text-xl md:text-3xl'>Fullstack Developer</h2>
-          <p className='text-sm py-4 text-gray-600 leading-6 md:text-xl'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit reiciendis eum voluptates eveniet suscipit omnis ab unde placeat distinctio nisi necessitatibus, ullam saepe id facilis ipsam molestias nihil neque. Eligendi?</p>
+          <h2 className='text-xl md:text-3xl'>
+            <Typewriter
+              options={{
+                strings: ['Mobile App Developer', 'Web Developer'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+            </h2>
+          <p className='text-sm py-4 text-gray-600 leading-6 md:text-xl'>I am a fast learner who adapts quickly to change and
+            eager to learn new methods and technologies, and work
+            as an effective team member. Intend to work smart for
+            the advancement of any project that I'm working on.</p>
           {/* social */}
           <div className='text-5xl flex justify-center gap-16 text-gray-700'>
             <a href="">
@@ -43,26 +58,32 @@ function Hero() {
               <AiFillLinkedin />
             </a>
             <a href="">
-              <AiOutlineWhatsApp />
+              <AiOutlineMail />
             </a>
           </div>
         </div>
         <div className="sm:basis-1/2 ">
           {/* profile */}
-          <div className=' m-auto'>
+          <motion.div
+
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+
+            className=' m-auto'>
             <Image
               src="/cvboynew.png "
               alt="cv"
-              width={800}
-              height={800}
+              width={700}
+              height={700}
             />
-          </div>
+          </motion.div>
         </div>
 
       </div>
 
     </section>
-    
+
 
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
 import Hero from './components/Hero'
 import { AiFillLinkedin, AiOutlineGithub, AiOutlineWhatsApp } from 'react-icons/ai'
+import { motion } from "framer-motion"
 
 
 export default function Home() {
   return (
-    <main className={'bg-blue-100 min-h-screen px-10 md:px-20 lg:px-40'}>
+    <main className={'bg-blue-100 min-h-screen px-10 md:px-20 lg:px-30'}>
       <Hero />
       <section>
         <div>
@@ -17,7 +19,12 @@ export default function Home() {
         {/* Services cards */}
         <div className=' md:flex lg:flex gap-10 mx-auto'>
           {/* card */}
-          <div className=' bg-blue-500 text-center shadow-sm p-5 rounded-xl my-10'>
+          <motion.div 
+          
+          whileHover={{ scale: 1.2 }}
+  onHoverStart={e => {}}
+  onHoverEnd={e => {}}
+          className=' bg-blue-500 text-center shadow-sm p-5 rounded-xl my-10'>
             <Image
               src="/developer.png "
               alt="cv"
@@ -27,8 +34,14 @@ export default function Home() {
             />
             <h2 className='text-2xl font-bold '>Developer</h2>
             <p className='text-sm py-2 text-gray-800 leading-6'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsa magni ad pariatur veniam repudiandae iusto perferendis eaque sapiente officia sint ratione et aperiam cupiditate modi amet non, id illum?</p>
-          </div>
-          <div className=' bg-blue-500 text-center shadow-sm p-5 rounded-xl my-10'>
+          </motion.div>
+
+          <motion.div 
+          whileHover={{ scale: 1.2 }}
+          onHoverStart={e => {}}
+          onHoverEnd={e => {}}
+          
+          className=' bg-blue-500 text-center shadow-sm p-5 rounded-xl my-10'>
             <Image
               src="/uiux-01.png "
               alt="cv"
@@ -38,8 +51,15 @@ export default function Home() {
             />
             <h2 className='text-2xl font-bold '>UI/UX Designer</h2>
             <p className='text-sm py-2 text-gray-800 leading-6'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsa magni ad pariatur veniam repudiandae iusto perferendis eaque sapiente officia sint ratione et aperiam cupiditate modi amet non, id illum?</p>
-          </div>
-          <div className=' bg-blue-500 text-center shadow-sm p-5 rounded-xl my-10'>
+          </motion.div>
+
+
+          <motion.div 
+          whileHover={{ scale: 1.2 }}
+          onHoverStart={e => {}}
+          onHoverEnd={e => {}}
+
+          className=' bg-blue-500 text-center shadow-sm p-5 rounded-xl my-10'>
             <Image
               src="/designer.png "
               alt="cv"
@@ -49,7 +69,7 @@ export default function Home() {
             />
             <h2 className='text-2xl font-bold '>Graphic Design</h2>
             <p className='text-sm py-2 text-gray-800 leading-6'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsa magni ad pariatur veniam repudiandae iusto perferendis eaque sapiente officia sint ratione et aperiam cupiditate modi amet non, id illum?</p>
-          </div>
+          </motion.div>
         </div>
       </section>
       {/* my works */}
@@ -59,75 +79,136 @@ export default function Home() {
           <p className='text-sm py-4 text-gray-600 leading-6'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit reiciendis eum voluptates eveniet suscipit omnis ab unde placeat distinctio nisi necessitatibus, ullam saepe id facilis ipsam molestias nihil neque. Eligendi?</p>
 
         </div>
-        {/* Images */}
-        <div className='flex flex-col gap-10 p-10 lg:flex-row lg:flex-wrap'>
+
+        {/* Images cards */}
+        <div className='flex flex-col gap-10  p-10 lg:flex-row lg:flex-wrap'>
           <div className='basis-1/3 flex-1'>
-          <Image
-            src="/Docpng.png "
-            alt="Medicare"
-            width={150}
-            height={150}
-            layout='responsive'
-            className='rounded-lg object-cover'
 
-          />
-           </div>
-           <div className='basis-1/3 flex-1'>
-          <Image
-            src="/Docpng.png "
-            alt="Medicare"
-            width={150}
-            height={150}
-            layout='responsive'
-            className='rounded-lg object-cover'
+            <div className="max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                <img className="rounded-t-lg" src="/Med.png" alt="" />
+              </a>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Read more
+                  <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </a>
+              </div>
+            </div>
 
-          />
-           </div>
-           <div className='basis-1/3 flex-1'>
-          <Image
-            src="/Docpng.png "
-            alt="Medicare"
-            width={150}
-            height={150}
-            layout='responsive'
-            className='rounded-lg object-cover'
+          </div>
+          <div className='basis-1/3 flex-1'>
 
-          />
-           </div>
-           <div className='basis-1/3 flex-1'>
-          <Image
-            src="/Docpng.png "
-            alt="Medicare"
-            width={150}
-            height={150}
-            layout='responsive'
-            className='rounded-lg object-cover'
+            <div className="max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                <img className="rounded-t-lg" src="MyPort.png" alt="" />
+              </a>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Read more
+                  <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </a>
+              </div>
+            </div>
 
-          />
-           </div>
-           <div className='basis-1/3 flex-1'>
-          <Image
-            src="/Docpng.png "
-            alt="Medicare"
-            width={150}
-            height={150}
-            layout='responsive'
-            className='rounded-lg object-cover'
+          </div>
+          <div className='basis-1/3 flex-1'>
 
-          />
-           </div>
-           <div className='basis-1/3 flex-1'>
-          <Image
-            src="/Docpng.png "
-            alt="Medicare"
-            width={150}
-            height={150}
-            layout='responsive'
-            className='rounded-lg object-cover'
+            <div className="max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                <img className="rounded-t-lg" src="/Tiny.png" alt="" />
+              </a>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Read more
+                  <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </a>
+              </div>
+            </div>
 
-          />
-           </div>
-          
+          </div>
+          <div className='basis-1/3 flex-1'>
+
+            <div className="max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                <img className="rounded-t-lg" src="/Docpng.png" alt="" />
+              </a>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Read more
+                  <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+          </div>
+          <div className='basis-1/3 flex-1'>
+
+            <div className="max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                <img className="rounded-t-lg" src="Sripng.png" alt="" />
+              </a>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Read more
+                  <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+          </div>
+          <div className='basis-1/3 flex-1'>
+
+            <div className="max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                <img className="rounded-t-lg" src="/port.png" alt="" />
+              </a>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Read more
+                  <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+          </div>
+
         </div>
 
       </section>
@@ -137,24 +218,24 @@ export default function Home() {
         <footer className='border-t-2 border-black text-sm opacity-70
          flex flex-col gap-5 items-center justify-center py-10 lg:flex-row lg:items-center'>
 
-        
-        <div>
-        <h3 className='text-base mb-2'>Contact me for more details</h3>
-        <p className='opacity-40'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ullam recusandae eius voluptatum saepe suscipit tenetur, temporibus quidem iste, autem officia, aliquid illum eos nemo quasi dolore. Neque, facere inventore?</p>
-        </div>
-        <div>
-        <div className='text-5xl flex justify-center gap-16 text-gray-700'>
-            <a href="">
-              <AiOutlineGithub />
-            </a>
-            <a href="">
-              <AiFillLinkedin />
-            </a>
-            <a href="">
-              <AiOutlineWhatsApp />
-            </a>
+
+          <div>
+            <h3 className='text-base mb-2'>Contact me for more details</h3>
+            <p className='opacity-40'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ullam recusandae eius voluptatum saepe suscipit tenetur, temporibus quidem iste, autem officia, aliquid illum eos nemo quasi dolore. Neque, facere inventore?</p>
           </div>
-        </div>
+          <div>
+            <div className='text-5xl flex justify-center gap-16 text-gray-700'>
+              <a href="">
+                <AiOutlineGithub />
+              </a>
+              <a href="">
+                <AiFillLinkedin />
+              </a>
+              <a href="">
+                <AiOutlineWhatsApp />
+              </a>
+            </div>
+          </div>
         </footer>
 
       </section>
